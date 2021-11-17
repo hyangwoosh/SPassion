ae8689240a094aa194380b4718181a2ad754f328
hyangwoosh <shine.thw@gmail.com>
2021-11-18 01:00:34 +0800

    Set up navigation for fragments

--- app/src/main/java/com/example/spassion/activities/MainActivity.java
+++ app/src/main/java/com/example/spassion/activities/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.spassion.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.spassion.R;
+import com.example.spassion.fragments.EventsFragment;
+import com.example.spassion.fragments.ExploreFragment;
+import com.example.spassion.fragments.HomeFragment;
+import com.example.spassion.fragments.ProfileFragment;
+import com.example.spassion.fragments.SignUpFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +22,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bottom nav
+        BottomNavigationView bottomNav = findViewById(R.id.nav_bottom);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        // Set default fragment when MainActivity first loads i.e. HomeFragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     } // End of onCreate
 
+    // This method is called in onCreate
+    private NavigationBarView.OnItemSelectedListener navListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.menu_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.menu_explore:
+                            selectedFragment = new ExploreFragment();
+                            break;
+                        case R.id.menu_sign_up:
+                            selectedFragment = new SignUpFragment();
+                            break;
+                        case R.id.menu_events:
+                            selectedFragment = new EventsFragment();
+                            break;
+                        case R.id.menu_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    };
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }; // End of onNavigationItemSelected
+            }; // End of NavigationBarView.OnItemSelectedListener
 
 } // End of MainActivity
--- app/src/main/java/com/example/spassion/fragments/EventsFragment.java
+++ app/src/main/java/com/example/spassion/fragments/EventsFragment.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.spassion.R;
 
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+import com.example.spassion.R;
+
+public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_events, container, false);
     } // End of onCreateView
 
-} // End of HomeFragment
+} // End of EventsFragment

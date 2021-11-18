9515a6b7f57ca3242a3dcb81136215b4688aef8d
hyangwoosh <shine.thw@gmail.com>
2021-11-18 18:31:12 +0800

    Update for Sign up fragment

--- app/src/main/java/com/example/spassion/fragments/SignUpFragment.java
+++ app/src/main/java/com/example/spassion/fragments/SignUpFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,7 +14,22 @@ import com.example.spassion.R;
 public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        //get the spinner from the xml.
+        Spinner dropdown = view.findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{
+                "SP Mixed Martial Arts", "SP Mind Sport", "SP Basketball", "SP Bowling", "SP Cyclists", "SP Badminton", "SP Adventurers"
+        };
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        return view;
+
     } // End of onCreateView
 
 } // End of SignUpFragment

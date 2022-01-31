d7b5f9f87b605596a411f2656873a0dc6f5d5934
hyangwoosh <shine.thw@gmail.com>
2022-01-31 10:47:09 +0800

    img mani

--- app/src/main/java/com/example/spassion/activities/ShopActivity.java
+++ app/src/main/java/com/example/spassion/activities/ShopActivity.java
@@ -47,8 +47,7 @@ public class ShopActivity extends AppCompatActivity {
         startActivity(new Intent(ShopActivity.this, MainActivity.class));
     }
 
-    public void sharpen(View view) {
-        operation = Bitmap.createBitmap(bmp.getWidth(),bmp.getHeight(), bmp.getConfig());
+    public void sharpen() {
         double[][] SharpConfig = new double[][] {
                 { 0 , -2    , 0  },
                 { -2, 11, -2 },
@@ -57,9 +56,8 @@ public class ShopActivity extends AppCompatActivity {
         ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
         convMatrix.applyConfig(SharpConfig);
         convMatrix.Factor = 3;
-
-        Bitmap someBmp = ConvolutionMatrix.computeConvolution3x3(operation, convMatrix);
-        im.setImageBitmap(someBmp);
+        Bitmap bmOut = ConvolutionMatrix.computeConvolution3x3(bmp, convMatrix);
+        im.setImageBitmap(bmOut);
     }
 
     public void doGamma(View view) {
@@ -115,26 +113,75 @@ public class ShopActivity extends AppCompatActivity {
         // return final image
         im.setImageBitmap(bmOut);
     }
+    public void doNormal(View view) {
+        setContentView(R.layout.activity_shop);
+//        mAdView = findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);
+        b1 = (Button) findViewById(R.id.button7);
+        im = (ImageView) findViewById(R.id.imageView4);
 
-    public void gray(View view) {
-        operation = Bitmap.createBitmap(bmp.getWidth(),bmp.getHeight(), bmp.getConfig());
-        double red = 0.33;
-        double green = 0.59;
-        double blue = 0.11;
+        BitmapDrawable abmp = (BitmapDrawable) im.getDrawable();
+        bmp = abmp.getBitmap();
+    }
 
-        for (int i = 0; i < bmp.getWidth(); i++) {
-            for (int j = 0; j < bmp.getHeight(); j++) {
-                int p = bmp.getPixel(i, j);
-                int r = Color.red(p);
-                int g = Color.green(p);
-                int b = Color.blue(p);
+    public void doNormal2(View view) {
+        // define rgb
+        red = 1;
+        green = 1;
+        blue = 1;
 
-                r = (int) red * r;
-                g = (int) green * g;
-                b = (int) blue * b;
-                operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
+        // create output image
+        Bitmap bmOut = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
+        // get image size
+        int width = bmp.getWidth();
+        int height = bmp.getHeight();
+        // color information
+        int A, R, G, B;
+        int pixel;
+        // constant value curve
+        final int    MAX_SIZE = 256;
+        final double MAX_VALUE_DBL = 255.0;
+        final int    MAX_VALUE_INT = 255;
+        final double REVERSE = 1.0;
+
+        // gamma arrays
+        int[] gammaR = new int[MAX_SIZE];
+        int[] gammaG = new int[MAX_SIZE];
+        int[] gammaB = new int[MAX_SIZE];
+
+        // setting values for every gamma channels
+        for(int i = 0; i < MAX_SIZE; ++i) {
+            gammaR[i] = (int)Math.min(MAX_VALUE_INT,
+                    (int)((MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / red)) + 0.5));
+            gammaG[i] = (int)Math.min(MAX_VALUE_INT,
+                    (int)((MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / green)) + 0.5));
+            gammaB[i] = (int)Math.min(MAX_VALUE_INT,
+                    (int)((MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / blue)) + 0.5));
+        }
+
+        // apply gamma table
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = bmp.getPixel(x, y);
+                A = Color.alpha(pixel);
+                // look up gamma
+                R = gammaR[Color.red(pixel)];
+                G = gammaG[Color.green(pixel)];
+                B = gammaB[Color.blue(pixel)];
+                // set new color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
             }
         }
+
+        // return final image
+        im.setImageBitmap(bmOut);
+    }
+
+    public void normal(View view) {
+        operation = Bitmap.createBitmap(bmp.getWidth(),bmp.getHeight(), bmp.getConfig());
+
         im.setImageBitmap(operation);
     }
 }
